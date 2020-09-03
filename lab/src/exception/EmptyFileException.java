41399130b32d6f9c997e9190d67db6ca0fdb659e
Pechenuca <48299507+Pechenuca@users.noreply.github.com>
2020-09-03 23:48:35 +0300

    ssss

--- lab/src/exception/EmptyFileException.java
+++ lab/src/exception/EmptyFileException.java
@@ -6,7 +6,6 @@ public class EmptyFileException extends IOException {
     /**
      * Конструктор - создает объект класса EmptyFileException
      * @param s - сообщение об исключении
-     * @see EmptyFileException#EmptyFileException(s)
      */
     public EmptyFileException(String s) {
         super(s);
